a48183a3cb9a6a9691dc9e0871dda16904b4c0cc
FarzadAb <farzad.abdolhosseini@gmail.com>
2015-11-12 12:05:27 +0330

    added createNode and deleteAll!

--- library/elasticServer.js
+++ library/elasticServer.js
@@ -8,16 +8,18 @@ var queryOptions = {
     path: '/db/data/cypher',
     port: '80',
     method: 'POST',
+    auth: 'admin:DVAzai94115',
     headers: {
-        'Host': 'node49444-bigdata.jelastic.elastx.net',
-        'Content-Length': '30',
-        'Authorization': 'Basic YWRtaW46RFZBemFpOTQxMTU=',
+        // 'Host': 'node49444-bigdata.jelastic.elastx.net',
+        // 'Content-Length': '30',
+        // 'Authorization': 'Basic YWRtaW46RFZBemFpOTQxMTU=',
     },
 };
 
 exp.query = function(query) {
     return new Promise(function(resolve, reject){
 
+        console.log(query);
         var output = '';
         var options = utils.clone(queryOptions);
         var params = JSON.stringify({
@@ -32,6 +34,7 @@ exp.query = function(query) {
             });
             res.on('end', function(data){
                 resolve(JSON.parse(output));
+                console.log('done!!!');
             });
         });
 
@@ -39,4 +42,34 @@ exp.query = function(query) {
 
         req.end();
     });
+};
+
+exp.deleteAll = function() {
+    exp.query('MATCH (x) - [z] -> (y) DELETE z');
+    exp.query('MATCH (n) DELETE n');
+};
+
+exp.createNode = function(nodeType, properties) {
+    var type, prop;
+    if( typeof(nodeType) === 'undefined' )
+        type = '';
+    else
+        type = ':' + nodeType;
+
+    if( typeof(properties) === 'undefined' )
+        props = '';
+    else {
+        props = ' {';
+        var first = true;
+        for(var p in properties) {
+            if( !first )
+                props += ',';
+            props += p + ':"' + properties[p] + '"';
+            first = false;
+        }
+        props += '}';
+    }
+
+    exp.query('CREATE (n' + type + props + ')');
+
 };
--- neo4j.js
+++ neo4j.js
@@ -1,5 +1,10 @@
 var server = require('./library/elasticServer.js');
+server.deleteAll();
+// server.createNode('news');
+// server.createNode('news', {name: 'asrnews'});
 
-var p = server.query('MATCH (n) RETURN n');
+// var q = 'CREATE (n {name:"World"}) RETURN "hello", n.name';
+// var p = server.query('MATCH (n) RETURN n');
 
-p.then(function(res) { console.log(res); });
+// p.then(function(res) { console.log(res); });
+// var fn_small = require('./data/FarsNews_Small.json').data;
